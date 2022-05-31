b438c32e5b47d72275c1f78c02634f47a91268d4
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-05-31 19:15:45 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-export const getSquaredNumbers = (arr) => arr.map((num) => num * num);
+export const getSquaredNumbers = (ar) => ar.map((num) => num * num);
 
 export const getOddNumbers = (arr) => arr.filter((num) => num % 2 === 1);
 
